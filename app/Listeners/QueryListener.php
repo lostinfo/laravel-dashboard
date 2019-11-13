2f2cd7c7e54ce36574a802e97e302af4127224ed
lostinfo <1225039937@qq.com>
2019-11-13 16:56:39 +0800

    QueryListener

--- app/Listeners/QueryListener.php
+++ app/Listeners/QueryListener.php
@@ -11,10 +11,12 @@ class QueryListener
     public function handle(QueryExecuted $executed)
     {
         if (config('app.debug')) {
-            $sql = str_replace("?", "'%s'", $executed->sql);
+            $sql = str_replace(array('%', '?'), array('%%', '%s'), $executed->sql);
             foreach ($executed->bindings as $key => $binding) {
                 if ($binding instanceof \DateTimeInterface) {
                     $executed->bindings[$key] = $binding->format('Y-m-d H:i:s');
+                } else if (is_string($binding)) {
+                    $executed->bindings[$key] = "'$binding'";
                 }
             }
             $log = vsprintf($sql, $executed->bindings) . " [query time: $executed->time ms]";
